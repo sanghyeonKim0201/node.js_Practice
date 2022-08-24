91c87d026603b073ca41fc0bea9b5755fad41e2b
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-08-24 22:25:11 +0900

    html, css 적용

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(8080, function(){
-    console.log('염해찬 병신');
+    console.log('start');
 });
 
-app.get('/beauty', function(요청, 응답){
-    응답.send('뷰티용품 쇼핑 페이지임');
+app.get('/beauty', function(req, res){
+    res.send('뷰티용품 쇼핑 페이지임');
+});
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/public/LoginExam.html')
 });
