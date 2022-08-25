b4703364ab15a67d6ff0e86d5755c757828ff383
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-08-25 14:40:00 +0900

    install mysql

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+const http = require('http');
+
 const express = require('express');
 const app = express();
 
+let port = 8080;
 app.use(express.static(__dirname + '/public'));
-
-app.listen(8080, function(){
-    console.log('start');
-});
 
 app.get('/beauty', function(req, res){
     res.send('뷰티용품 쇼핑 페이지임');
@@ -13,3 +12,10 @@ app.get('/beauty', function(req, res){
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/public/LoginExam.html')
 });
+
+const server = http.createServer(app);
+
+server.listen(port, function(){
+    console.log('start');
+});
+
