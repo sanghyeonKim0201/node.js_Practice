998d9899f371e03c240caf15ba352a3890c2a1ae
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-08-25 17:17:06 +0900

    add library

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const http = require('http');
-
 const express = require('express');
 const app = express();
+const winston = require('winston');
+const winstonDaily = require('winston-daily-rotate-file');
+const appRoot = require('app-root-path'); 
 
 let port = 8080;
 app.use(express.static(__dirname + '/public'));
 
-app.get('/beauty', function(req, res){
-    res.send('뷰티용품 쇼핑 페이지임');
-});
-app.get('/', function(req, res){
-    res.sendfile(__dirname + '/public/LoginExam.html')
-});
-
-const server = http.createServer(app);
-
-server.listen(port, function(){
+const server = app.listen(port, ()=>{
     console.log('start');
 });
 
+app.get('/beauty', (req, res) =>{
+    res.send('뷰티용품 쇼핑 페이지임');
+});
+
+app.get('/', (req,res)=>{
+    res.sendfile(__dirname + '/public/LoginExam.html')
+});
