d043666121d0f05f36c0a0c520adb94867748c6b
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-11-07 21:01:44 +0900

    reset server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const app = express();
-const winston = require('winston');
-const winstonDaily = require('winston-daily-rotate-file');
-const appRoot = require('app-root-path'); 
 
-let port = 8080;
-app.use(express.static(__dirname + '/public'));
+const PORT = 8080;
 
-const server = app.listen(port, ()=>{
-    console.log('start');
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.listen(PORT, ()=>{
+    console.log("server start");
 });
 
-app.get('/sing', (req, res) =>{
-    res.send('뷰티용품 쇼핑 페이지임');
+app.get("/", (req, res) =>{
+    res.render("home/index");
 });
-
-app.get('/Login', (req,res)=>{
-    res.sendfile(__dirname + '/public/Login/LoginExam.html')
+app.get("/login", (req, res) =>{
+    res.render("home/login");
 });
