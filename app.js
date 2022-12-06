4b6c919f594f28fd3b81fc132ad949196e73020e
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-12-06 19:56:16 +0900

    MVC

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const PORT = 8080;
+const home = require("./src/routes/home")
+app.use("/", home);
 
-app.set("views", "./views");
+app.set("views", __dirname + "/src/views");
 app.set("view engine", "ejs");
+app.set()
 
-const home = require("./routes/home");
-
-app.use("/", home); //use -> 미들웨어 등록해주는 메소드
-
-app.listen(PORT, ()=>{
-    console.log("server start");
-});
-
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,7 @@
+const app = require("../app.js");
+
+const PORT = 8080;
+
+app.listen(PORT, ()=>{
+    console.log("start");
+});
