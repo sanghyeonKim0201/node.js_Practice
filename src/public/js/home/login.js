4c408d2800f2a3845a611862ff355533d6246a61
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-12-08 08:29:53 +0900

    javascript - ejs

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -3,11 +3,9 @@ const id = document.querySelector("#id");
 const pw = document.querySelector("#pw");
 const login = document.querySelector(".login");
 
-loginEvent = function(){
+login.addEventListener("click", ()=>{
     console.log("hi");
-}
-
-login.addEventListener("click", loginEvent);
+});
 
 
 
