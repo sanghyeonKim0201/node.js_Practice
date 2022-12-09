67347afbfde682e39a454c58903520e8eed0727d
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-12-09 13:33:38 +0900

    login_API_10%

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -10,21 +10,12 @@ const output = {
     },
 };
 
-const user = {
-    id: ["qwer", "asdf", "zxcv"],
-    pw: ["1234", "5678", "9101"]
-};
-
 const process = {
     login: (req, res)=>{
         const id = req.body.id,
         pw = req.body.pw
 
-        if(user.id.includes(id) && user.pw.includes(pw)){
-           
-        }else{
-           console.log 
-        }
+        const db = require("")
     }
 }
 
